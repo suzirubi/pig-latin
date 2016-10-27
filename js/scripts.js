84856da83315648a0cd908620850acb147f2e7c7
Claire Sial and Suzi Rubino <suzi@>
2016-10-26 18:43:40 -0700

    add for branching to change single letter words

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 // business logic
-arraySentence.forEach(word,)
-
-
-
 
 
 $(document).ready(function() {
 
   $("form#pigForm").submit(function(event) {
 
-      var originalSentence = $("input#pigInput").val();
-      var arraySentence = originalSentence.split(" ");
+    var originalSentence = $("input#pigInput").val();
+    var arraySentence = originalSentence.split(" ");
+    var oneWord = [];
 
-      // var result = translate(originalSentence);
-      //
-      // $(".result").text(result);
+    for (var count = 0; count < arraySentence.length; count += 1) {
+      var oneWord = arraySentence.map(function(count) {
+        if (count === "A") {
+          return "Aay";
+        } else if (count === "a") {
+          return "aay";
+        } else if (count === "I") {
+          return "Iay";
+        } else if (count === "i") {
+          return "iay";
+        } else if (count === "O") {
+          return "Oay";
+        } else {
+          return count;
+        }
+      });
+    }
+
+    var result = oneWord.join(" ");
+
+    $(".result").text(result);
 
     event.preventDefault();
   });
